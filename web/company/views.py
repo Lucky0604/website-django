0bc4fd8e1558792c97f4a2c25535c5586e9ebb1b
Lucky <175917645@qq.com>
2016-05-13 00:41:50 +0800

    add views

--- web/company/views.py
+++ web/company/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Company, Post
 # Create your views here.
+
+def company_list(request):
+    tags = Company.objects.all()
+    return render(request, 'company/company_list.html', {'tags': tags})
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'company/post_list.html', {'posts': posts})
