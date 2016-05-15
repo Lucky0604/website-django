dc6a15145d8ac0ec021292ef646769d11a0a0663
Lucky <175917645@qq.com>
2016-05-15 15:49:25 +0800

    set up the layout and urls

--- web/company/urls.py
+++ web/company/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name = 'index'),
     url(r'^main/', views.main_list, name = 'main_list'),
+    url(r'^company_main', views.company_main, name = 'company_main'),
 ]
--- web/company/views.py
+++ web/company/views.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from .models import Company, Author, Content
 # Create your views here.
 
+def index(request):
+    return render(request, 'index.html', {})
+
+def company_main(request):
+    return render(request, 'company_main.html', {})
+
 def main_list(request):
     companys = Company.objects.all()
     contents = Content.objects.all()
