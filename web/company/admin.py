39ec2290737835637d86bc4bc04af75f941c41a0
Lucky <175917645@qq.com>
2016-05-18 00:17:13 +0800

    list app detail

--- web/company/admin.py
+++ web/company/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.utils import timezone
 from .models import Company, Author, Content
 # Register your models here.
-admin.site.register(Company)
-admin.site.register(Author)
-admin.site.register(Content)
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('tag', 'tagline')
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'company_name', 'created_date', 'pub_date')
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Content, ContentAdmin)
